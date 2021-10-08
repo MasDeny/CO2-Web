84bb85d73baa210caff4395605242999aae920a2
Imanindra Permana <imanindrapermana@gmail.com>
2021-10-08 15:03:21 +0700

    Thanks Page

---
+++ pages/thanks/thanks-page.php
@@ -0,0 +1,28 @@
+<div class="container">
+    <div class="thanks-page">
+        <div>
+            <div class="text-center">
+                <img class="img-fluid" src="../assets/img/about.png">
+            </div>
+            <div class="text-center">
+                <h1 class="font-weight-bold text-uppercase text-black">Thank you for contacting us!</h1>
+            </div>
+            <div class="text-center">
+                <h5 class="text-black">We have received your message.</h5>
+                <h5 class="text-black mb-5">We`ll reach you out immediately!</h5>
+            </div>
+            <div class="text-center">
+                <a href="home" class="btn bg-yellow btn-rounded text-uppercase text-black font-weight-bold mb-4">back to homepage</a>
+            </div>
+            <div class="text-center">
+                <h5 class="font-weight-bold text-yellow mb-4">Let`s connect !</h5>
+                <div class="facebook d-inline-block mr-3">
+                    <a href="https://www.facebook.com/codingcollective/" target="blank"> <img src="../assets/img/icon/facebook-hover.png"></a>
+                </div>
+                <div class="facebook d-inline-block">
+                    <a href="https://www.linkedin.com/company/codingcollective/" target="blank"> <img src="../assets/img/icon/linked-in-hover.png"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
